0f686a570269999107c94177397fb607db579981
Lucas Aguiar <Lucas.Nune@outlook.com.br>
2022-09-06 14:42:05 -0300

    Alterações no menu central

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,42 +3,44 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("-----------------------");
-        System.out.println("♦ Rebase again! ♦");
-        System.out.println("-----------------------");
+
+        int response;
+        Scanner readResponse = new Scanner(System.in);
+
+        OperadoresAritmeticos ResponseOperadores = new OperadoresAritmeticos();
+        TiposDeDados ResponseTiposDeDados = new TiposDeDados();
+
+        System.out.println("|------------------------------------------------------------------------------------------------------------------|");
+        System.out.println("|                                     *  Seja Bem vindo ao Livrinho do Lucas *                                     |");
+        System.out.println("|------------------------------------------------------------------------------------------------------------------|");
         System.out.println();
-
-        System.out.println("-----------------------");
-        System.out.println("♦ Tipos de dados ♦");
-        System.out.println("-----------------------");
-
-        TiposDeDados ExempTipoDeDados = new TiposDeDados();
-
-        System.out.println(ExempTipoDeDados.getInteiros());
-        System.out.println("--------------------------------------------------");
-        System.out.println(ExempTipoDeDados.getPontosFlutuantes());
-        System.out.println("--------------------------------------------------");
-        System.out.println(ExempTipoDeDados.getBooleanos());
-        System.out.println("--------------------------------------------------");
-        System.out.println(ExempTipoDeDados.getCaracteres());
-        System.out.println("--------------------------------------------------");
+        System.out.println("|------------------------------------------------------------------------------------------------------------------|");
+        System.out.println("| * Digite o número do conteudo que deseja ver                                                                     |");
+        System.out.println("| * Tipos de Dados (1)                                                                                             |");
+        System.out.println("| * Operadores Aritméticos (2)                                                                                     |");
+        System.out.println("|------------------------------------------------------------------------------------------------------------------|");
         System.out.println();
+        System.out.print("Escolha: ");
+        response = readResponse.nextInt();
 
-        System.out.println("--------------------------------------------------");
-        System.out.println("♦ (CAPACIDADE DE INTEIROS) ♦");
-        System.out.println("--------------------------------------------------");
-        System.out.println(ExempTipoDeDados.getCapacidadeInteiros());
-        System.out.println();
-
-        System.out.println("|-----------------------------------------------------------------|");
-        System.out.println("|           (CONVERSÕES AUTOMATICAS DE TIPOS DE DADOS)            |");
-        System.out.println("|-----------------------------------------------------------------|");
-        ExempTipoDeDados.getConvercaoAutomaticaDeTiposDeDados();
-
-        System.out.println();
-        System.out.println("|-----------------------------------------------------------------|");
-        System.out.println("|                    (OPERADORES ARITMÉTICOS)                     |");
-        System.out.println("|-----------------------------------------------------------------|");
-        ExempTipoDeDados.getTabelaOperadores();
+        if(response == 1){
+            System.out.println("|------------------------------------------------------------------------------------------------------------------|");
+            System.out.println("| * Digite o número do conteudo que deseja ver                                                                     |");
+            System.out.println("| * Tabela Tipos De Dados (1)                                                                                      |");
+            System.out.println("| * Tabela Capacidade Inteiros (2)                                                                                 |");
+            System.out.println("| * Convercao Automatica De Tipos De Dados (3)                                                                     |");
+            System.out.println("|------------------------------------------------------------------------------------------------------------------|");
+            System.out.print("Escolha: ");
+            response = readResponse.nextInt();
+            if(response == 1){
+                ResponseTiposDeDados.getTabelaTiposDeDados();
+            }else if(response == 2){
+                ResponseTiposDeDados.getTabelaCapacidadeInteiros();
+            }else if(response == 3){
+                ResponseTiposDeDados.getConvercaoAutomaticaDeTiposDeDados();
+            }
+        }else if(response == 2){
+            ResponseOperadores.getTabelaOperadores();
+        }
     }
 }
